71c2db47cf98460baf12f7ef34f00daceb2e32fd
aflori04123 <108504682+aflori04123@users.noreply.github.com>
2023-08-03 18:12:08 -0500

    Update pandasPractice1.py

--- pandasPractice1.py
+++ pandasPractice1.py
@@ -5,4 +5,16 @@ myseries = pd.Series(
      index = ["a","b","c"]
 )
 
+<<<<<<< Updated upstream
 print(myseries)
+=======
+     def findAverages(self):
+        self.averages  = self.data.iloc[:, 2:].mean()
+
+     def printAverages(self):
+         print(self.averages)
+a = Parser()
+a.readData("sales.csv")
+a.findAverages()
+a.printAverages()
+>>>>>>> Stashed changes
